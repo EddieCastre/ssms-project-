4b20aea3cc610ee919c0c14996e46689144a7aaf
Ryzen <Ryzen@User>
2026-02-17 18:12:03 +0800

    try

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -5,12 +5,8 @@
 package javaapplication2;
 
 public class JavaApplication2 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+  
     }
     
 }
